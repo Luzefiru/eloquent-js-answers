43fb98edbc2c38505a1a9da80074ec54d50ab5d0
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-08 10:16:40 +0800

    feat: finish ch5/01.js

---
+++ ch5/01.js
@@ -0,0 +1,8 @@
+/*
+Use the reduce method in combination with the concat method to “flatten”
+an array of arrays into a single array that has all the elements of the original
+arrays.
+*/
+
+const flatten = (arr) =>
+  arr.reduce((accumulator, e) => accumulator.concat(e), []);
